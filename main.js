ffe403e06f8c3e3abeae02de60d25285b96c20e2
Yunuen AM <yunameza@outlook.com>
2024-02-01 13:41:32 -0600

    ✨Feat: work-section cards done

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const scrollToWorkSection = ()=> {
     document.getElementById('work-section').scrollIntoView({ behavior: 'smooth' });
   }
+
+  const scrollToHome = ()=> {
+    document.getElementById('background-section').scrollIntoView({ behavior: 'smooth' });
+  }
   
